2f033edae065eca39b0ff158f8eb461c6cee4776
msufyanking06-cpu <msufyanking06@gmail.com>
2026-07-08 20:19:33 +0500

    Update Testimonials.jsx

--- src/Apis/Testimonials.jsx
+++ src/Apis/Testimonials.jsx
@@ -7,13 +7,13 @@ const Testimonials = [
   },
   {
     id: 2,
-    image : "/images/elipse_1_2.png",
+    image : "../assets/images/elipse_1_2.png",
     name: "Jhon Smiteh",
     description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
   },
   {
     id: 3,
-    image : "/images/elipse_1.png",
+    image : "../assets/images/elipse_1.png",
     name: "Sara Smiteh",
     description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
   }
