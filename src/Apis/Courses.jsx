2294f442f30efd1c99809a102a0f47211b790364
msufyanking06-cpu <msufyanking06@gmail.com>
2026-07-08 19:25:46 +0500

    Add files via upload

---
+++ src/Apis/Courses.jsx
@@ -0,0 +1,32 @@
+
+const Courses = [
+  {
+    id: 1,
+    title: "Course 01",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic vel, recusandae fugit enim, dolor aperiam commodi esse dolorem.",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Course 02",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    link: "#",
+  },
+ {
+    id: 3,
+    title: "Course 03",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    link: "#",
+  }, {
+    id: 4,
+    title: "Course 04",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    link: "#",
+  }
+];
+
+export default Courses
---
+++ src/Apis/Testimonials.jsx
@@ -0,0 +1,22 @@
+const Testimonials = [
+  {
+    id: 1,
+    image : "/images/elipse_1_3.png",
+    name: "Laura Smith",
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
+  },
+  {
+    id: 2,
+    image : "/images/elipse_1_2.png",
+    name: "Jhon Smiteh",
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
+  },
+  {
+    id: 3,
+    image : "/images/elipse_1.png",
+    name: "Sara Smiteh",
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
+  }
+];
+
+export default Testimonials;
